49c685e633ea996de092feefea8c774cb3105c7c
Phạm Khắc Đô <phamdo1132@gmail.com>
2026-05-13 00:42:09 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# ==========================================
-# THÔNG TIN REPO CỦA BẠN (pkd11011)
-# ==========================================
-MY_USER="pkd11011"
-MY_REPO="Xrayr0.9.5"
-MY_TAG="v0.9.5"
-# ==========================================
-
 red='\033[0;31m'
 green='\033[0;32m'
 yellow='\033[0;33m'
@@ -16,7 +8,7 @@ plain='\033[0m'
 cur_dir=$(pwd)
 
 # check root
-[[ $EUID -ne 0 ]] && echo -e "${red}Lỗi:${plain} Bạn phải dùng quyền root để chạy script này!\n" && exit 1
+[[ $EUID -ne 0 ]] && echo -e "${red}错误：${plain} 必须使用root用户运行此脚本！\n" && exit 1
 
 # check os
 if [[ -f /etc/redhat-release ]]; then
@@ -34,7 +26,7 @@ elif cat /proc/version | grep -Eqi "ubuntu"; then
 elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
     release="centos"
 else
-    echo -e "${red}Không nhận diện được hệ điều hành!${plain}\n" && exit 1
+    echo -e "${red}未检测到系统版本，请联系脚本作者！${plain}\n" && exit 1
 fi
 
 arch=$(arch)
@@ -47,19 +39,38 @@ elif [[ $arch == "s390x" ]]; then
     arch="s390x"
 else
     arch="64"
-    echo -e "${red}Phát hiện kiến trúc thất bại, dùng mặc định x64${plain}"
+    echo -e "${red}检测架构失败，使用默认架构: ${arch}${plain}"
 fi
 
-echo "Kiến trúc CPU: ${arch}"
+echo "架构: ${arch}"
 
 if [ "$(getconf WORD_BIT)" != '32' ] && [ "$(getconf LONG_BIT)" != '64' ] ; then
-    echo "Phần mềm này chỉ hỗ trợ hệ điều hành 64-bit."
+    echo "本软件不支持 32 位系统(x86)，请使用 64 位系统(x86_64)，如果检测有误，请联系作者"
     exit 2
 fi
 
 os_version=""
+
+# os version
 if [[ -f /etc/os-release ]]; then
     os_version=$(awk -F'[= ."]' '/VERSION_ID/{print $3}' /etc/os-release)
+fi
+if [[ -z "$os_version" && -f /etc/lsb-release ]]; then
+    os_version=$(awk -F'[= ."]+' '/DISTRIB_RELEASE/{print $2}' /etc/lsb-release)
+fi
+
+if [[ x"${release}" == x"centos" ]]; then
+    if [[ ${os_version} -le 6 ]]; then
+        echo -e "${red}请使用 CentOS 7 或更高版本的系统！${plain}\n" && exit 1
+    fi
+elif [[ x"${release}" == x"ubuntu" ]]; then
+    if [[ ${os_version} -lt 16 ]]; then
+        echo -e "${red}请使用 Ubuntu 16 或更高版本的系统！${plain}\n" && exit 1
+    fi
+elif [[ x"${release}" == x"debian" ]]; then
+    if [[ ${os_version} -lt 8 ]]; then
+        echo -e "${red}请使用 Debian 8 或更高版本的系统！${plain}\n" && exit 1
+    fi
 fi
 
 install_base() {
@@ -72,6 +83,7 @@ install_base() {
     fi
 }
 
+# 0: running, 1: not running, 2: not installed
 check_status() {
     if [[ ! -f /etc/systemd/system/XrayR.service ]]; then
         return 2
@@ -84,64 +96,119 @@ check_status() {
     fi
 }
 
+install_acme() {
+    curl https://get.acme.sh | sh
+}
+
 install_XrayR() {
     if [[ -e /usr/local/XrayR/ ]]; then
         rm /usr/local/XrayR/ -rf
     fi
 
     mkdir /usr/local/XrayR/ -p
-    cd /usr/local/XrayR/
+	cd /usr/local/XrayR/
 
-    # Luôn cài bản v0.9.5 từ Repo của bạn
-    last_version="${MY_TAG}"
-    
-    echo -e "Bắt đầu tải XrayR từ nguồn cá nhân: ${MY_USER}/${MY_REPO}"
-    wget -q -N --no-check-certificate -O /usr/local/XrayR/XrayR-linux.zip https://github.com/${MY_USER}/${MY_REPO}/releases/download/${last_version}/XrayR-linux-${arch}.zip
-    
-    if [[ $? -ne 0 ]]; then
-        echo -e "${red}Tải file thất bại! Hãy chắc chắn bạn đã upload đủ 49 file Assets vào Release ${MY_TAG}.${plain}"
-        exit 1
+    if  [ $# == 0 ] ;then
+        # Thay đổi: Trỏ API về repo pkd11011/Xrayr0.9.5
+        last_version=$(curl -Ls "https://api.github.com/repos/pkd11011/Xrayr0.9.5/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+        if [[ ! -n "$last_version" ]]; then
+            echo -e "${red}检测 XrayR 版本失败，请先在 https://github.com/pkd11011/Xrayr0.9.5/releases 发布包含 XrayR-linux-${arch}.zip 的 Release${plain}"
+            exit 1
+        fi
+        echo -e "检测到 XrayR 最新版本：${last_version}，开始安装"
+        # Thay đổi: Link download từ pkd11011
+        wget -q -N --no-check-certificate -O /usr/local/XrayR/XrayR-linux.zip https://github.com/pkd11011/Xrayr0.9.5/releases/download/${last_version}/XrayR-linux-${arch}.zip
+        if [[ $? -ne 0 ]]; then
+            echo -e "${red}下载 XrayR 失败，请确认 https://github.com/pkd11011/Xrayr0.9.5/releases/download/${last_version}/XrayR-linux-${arch}.zip 存在${plain}"
+            exit 1
+        fi
+    else
+        if [[ $1 == v* ]]; then
+            last_version=$1
+	else
+	    last_version="v"$1
+	fi
+        # Thay đổi: Link download từ pkd11011
+        url="https://github.com/pkd11011/Xrayr0.9.5/releases/download/${last_version}/XrayR-linux-${arch}.zip"
+        echo -e "开始安装 XrayR ${last_version}"
+        wget -q -N --no-check-certificate -O /usr/local/XrayR/XrayR-linux.zip ${url}
+        if [[ $? -ne 0 ]]; then
+            echo -e "${red}下载 XrayR ${last_version} 失败，请确认 ${url} 存在${plain}"
+            exit 1
+        fi
     fi
 
     unzip XrayR-linux.zip
     rm XrayR-linux.zip -f
     chmod +x XrayR
     mkdir /etc/XrayR/ -p
-    
-    # Tải file service từ repo của bạn
     systemctl unmask XrayR 2>/dev/null
     rm /etc/systemd/system/XrayR.service -f
-    file_service="https://raw.githubusercontent.com/${MY_USER}/${MY_REPO}/master/XrayR.service"
-    wget -q -N --no-check-certificate -O /etc/systemd/system/XrayR.service ${file_service}
+    # Thay đổi: Tải file service từ pkd11011
+    file="https://raw.githubusercontent.com/pkd11011/Xrayr0.9.5/master/XrayR.service"
+    wget -q -N --no-check-certificate -O /etc/systemd/system/XrayR.service ${file}
     
     systemctl daemon-reload
     systemctl stop XrayR
     systemctl enable XrayR
-    
-    echo -e "${green}XrayR ${last_version}${plain} đã cài đặt thành công."
-    
-    # Copy các file database nếu có
-    [[ -f geoip.dat ]] && cp geoip.dat /etc/XrayR/
-    [[ -f geosite.dat ]] && cp geosite.dat /etc/XrayR/ 
+    echo -e "${green}XrayR ${last_version}${plain} 安装完成，已设置开机自启"
+    cp geoip.dat /etc/XrayR/
+    cp geosite.dat /etc/XrayR/ 
 
     if [[ ! -f /etc/XrayR/config.yml ]]; then
         cp config.yml /etc/XrayR/
+        echo -e ""
+        echo -e "全新安装，请先参看教程: https://github.com/pkd11011/Xrayr0.9.5"
+    else
+        systemctl start XrayR
+        sleep 2
+        check_status
+        echo -e ""
+        if [[ $? == 0 ]]; then
+            echo -e "${green}XrayR 重启成功${plain}"
+        else
+            echo -e "${red}XrayR 可能启动失败，请稍后使用 XrayR log 查看日志信息${plain}"
+        fi
     fi
 
-    # Tải script quản lý XrayR.sh từ repo của bạn
-    curl -fLo /usr/bin/XrayR https://raw.githubusercontent.com/${MY_USER}/${MY_REPO}/master/XrayR.sh
+    if [[ ! -f /etc/XrayR/dns.json ]]; then
+        cp dns.json /etc/XrayR/
+    fi
+    if [[ ! -f /etc/XrayR/route.json ]]; then
+        cp route.json /etc/XrayR/
+    fi
+    if [[ ! -f /etc/XrayR/custom_outbound.json ]]; then
+        cp custom_outbound.json /etc/XrayR/
+    fi
+    if [[ ! -f /etc/XrayR/custom_inbound.json ]]; then
+        cp custom_inbound.json /etc/XrayR/
+    fi
+    if [[ ! -f /etc/XrayR/rulelist ]]; then
+        cp rulelist /etc/XrayR/
+    fi
+    # Thay đổi: Tải file shell quản lý từ pkd11011
+    curl -fLo /usr/bin/XrayR https://raw.githubusercontent.com/pkd11011/Xrayr0.9.5/master/XrayR.sh
+    if [[ $? -ne 0 ]]; then
+        echo -e "${red}下载 XrayR 管理脚本失败，请检查 raw link${plain}"
+        exit 1
+    fi
     chmod +x /usr/bin/XrayR
-    ln -sf /usr/bin/XrayR /usr/bin/xrayr 
+    ln -sf /usr/bin/XrayR /usr/bin/xrayr
     chmod +x /usr/bin/xrayr
-    
-    systemctl start XrayR
-    
     cd $cur_dir
     rm -f install.sh
     echo -e ""
-    echo "Cài đặt hoàn tất! Gõ 'XrayR' để mở menu quản lý."
+    echo "XrayR 管理脚本使用方法: "
+    echo "------------------------------------------"
+    echo "XrayR                    - 显示管理菜单"
+    echo "XrayR start              - 启动 XrayR"
+    echo "XrayR stop               - 停止 XrayR"
+    echo "XrayR restart            - 重iq XrayR"
+    echo "XrayR status             - 查看 XrayR 状态"
+    echo "XrayR log                - 查看 XrayR 日志"
+    echo "------------------------------------------"
 }
 
-echo -e "${green}Bắt đầu cài đặt XrayR (Nguồn: ${MY_USER})${plain}"
+echo -e "${green}开始安装 (Nguồn pkd11011)${plain}"
 install_base
-install_XrayR
+install_XrayR $1
